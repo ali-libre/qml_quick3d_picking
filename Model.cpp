c7c021b9c7387a92ad891736fb56dae285eeb066
ali <waterwhale@chmail.ir>
2022-10-06 09:13:40 +0330

    add HFM Reader

--- Model.cpp
+++ Model.cpp
@@ -2,21 +2,26 @@
 //#include <Q
 Model::Model(QObject *parent) : QObject{parent} {
     connect(TestJsonTimer, &QTimer::timeout, this, &Model::change_Rotation);
-    TestJsonTimer->start(200);
+    TestJsonTimer->start(40);
     QJsonObject property;
     QJsonObject query;
+    QJsonObject query2;
 //    QJsonObject LED;
 
-
+// TODO: CLeanUp Code
     QJsonArray jArr;
     for (int Loop = 0; Loop < 10; Loop++) {
         property["x"] = 200 * Loop;
-        property["y"] = 0;
+        if(Loop %2)
+            property["y"] = 0;
+        else
+            property["y"] = 400;
         property["z"] = 0;
         property["pump"] = 200;
         property["rX"] = 0;
         property["rY"] = 0;
         property["rZ"] = 0;
+        property["color"] = "#FF0000";
 
 //        LED["LED"] = property;
 //        jArr.append(LED);
@@ -24,7 +29,11 @@ Model::Model(QObject *parent) : QObject{parent} {
     }
     //    test["position"] =
     query["nozzle"] = jArr;
+    query2["nozzle"] = jArr;
     jsonData.setObject(query);
+    jsonData.setObject(query2);
+    readHFM("/home/minoosa/Documents/Robot Test Fountain.hfm");
+    update_model();
     //    for (int Loop = 0; Loop < 10; Loop++) {
     //        addObject();
     //    }
@@ -33,8 +42,11 @@ Model::Model(QObject *parent) : QObject{parent} {
 }
 
 const QJsonDocument &Model::getJsonData() const {
-//    qDebug() << jsonData.toJson ();
-    return jsonData;
+    //    qDebug() << jsonData.toJson ();
+    //    return jsonData;
+//    resultmodel.setObject (jObjFountain);
+    QJsonDocument resultmodel(jObjFountain);
+    return resultmodel;
 }
 
 //QString Model::getPlainJsonData() {
@@ -67,9 +79,149 @@ void Model::setPlainJsonData(const QByteArray &newPlainJsonData)
     emit plainJsonDataChanged();
 }
 
+bool Model::readHFM(QString path) {
+    QJsonObject obj;
+    path.remove("file://");
+    obj = file2Jobj(path);
+    QString fountainName = obj["Fountain Name"].toString();
+    if(!fountainName.size ())
+        return false;
+    if (obj.isEmpty())
+        return false;
+    jObjFountain = obj;
+    return true;
+}
+
+QJsonObject Model::loadJsonObject(QString title, QString extention) {
+    QJsonObject obj;
+    //    QString loadPath =
+    //        File->getOpenFileName(NULL, title, documentPath, extention);
+    //    obj = file2Jobj(loadPath);
+    return obj;
+}
+
+QJsonObject Model::file2Jobj(QString f) {
+    QJsonObject obj;
+    QFile file(f);
+    //    file.remove("file://");
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "Could Not Open File...";
+        return obj;
+    }
+    QByteArray inputProject = file.readAll();
+    QJsonDocument loadDoc(QJsonDocument::fromJson(inputProject));
+    obj = loadDoc.object();
+    file.close();
+    return obj;
+}
+
+void Model::update_model() {
+    //    QJsonArray JArrayGroups;
+    QJsonArray nozzelList;
+    //    JArrayGroups = jObjFountain["Group"].toArray();
+
+    int groupY = -500;
+    for (auto jagrp : jObjFountain["Group"].toArray()) {
+        QJsonObject jObjGroup = jagrp.toObject();
+        QJsonArray jArrayNozzel = jObjGroup["Nozzles"].toArray();
+        int groupX = -1000;
+        for (auto nzl : jArrayNozzel) {
+            QJsonObject jObjNozzel = nzl.toObject();
+            QJsonArray jArrayLED = jObjNozzel["LED List"].toArray();
+            QJsonArray jArrayPump = jObjNozzel["Pump List"].toArray();
+            QJsonArray jArrayRobot = jObjNozzel["Robot List"].toArray();
+            QJsonArray tempNozzel;
+            //            if (jArrayLED.count() > jArrayPump.count())
+            for (auto jaled : jArrayLED) {
+                QJsonObject jObjLED = jaled.toObject();
+                QJsonObject Nozzel;
+                Nozzel["NID"] = jObjLED["NID"];
+                Nozzel["GID"] = jObjGroup["ID"];
+                Nozzel["x"] = groupX;
+                Nozzel["y"] = groupY;
+                Nozzel["z"] = 0;
+                Nozzel["pump"] = 200;
+                Nozzel["rX"] = 0;
+                Nozzel["rY"] = 0;
+                Nozzel["rZ"] = 0;
+                Nozzel["color"] = "#FF0000";
+                nozzelMerge(Nozzel, jObjLED, "L");
+                tempNozzel.append(Nozzel);
+                groupX += 100;
+            }
+
+            // Pump Handle
+            if(jArrayPump.count())
+            if (jArrayPump.count() < jArrayLED.count()) {
+                for (auto tnozzel : tempNozzel) {
+                    QJsonObject obj = tnozzel.toObject();
+                    tnozzel = nozzelMerge(obj, jArrayPump[0].toObject(), "P");
+                }
+            } else if (jArrayPump.count() == jArrayLED.count()) {
+                int Loop = 0;
+                for (auto tnozzel : tempNozzel) {
+                    QJsonObject obj = tnozzel.toObject();
+                    tnozzel =
+                        nozzelMerge(obj, jArrayPump[Loop++].toObject(), "P");
+                }
+            }
+
+            // Robot Handle
+            if(jArrayRobot.count())
+            if (jArrayRobot.count() < jArrayLED.count()) {
+                for (auto tnozzel : tempNozzel) {
+                    QJsonObject obj = tnozzel.toObject();
+                    tnozzel = nozzelMerge(obj, jArrayRobot[0].toObject(), "R");
+                }
+            } else if (jArrayRobot.count() == jArrayLED.count()) {
+                int Loop = 0;
+                for (auto tnozzel : tempNozzel) {
+                    QJsonObject obj = tnozzel.toObject();
+                    tnozzel =
+                        nozzelMerge(obj, jArrayRobot[Loop++].toObject(), "R");
+                }
+            }
+            groupX += 100;
+
+            // fill array
+            for (auto tNozzel : tempNozzel)
+                nozzelList.append(tNozzel.toObject());
+        }
+        groupY += 200;
+    }
+//        for (auto tnozzel : nozzelList) {
+//            QJsonObject obj = tnozzel.toObject();
+//            obj["rX"] = 0;
+//            obj["rY"] = 0;
+//            obj["rZ"] = 0;
+//        }
+    qDebug() << nozzelList;
+    QJsonObject testSubobj;
+    testSubobj["nozzle"] = nozzelList;
+    jObj3DMap["test"] = testSubobj;
+    jObj3DMap["nozzle"] = nozzelList;
+}
+
+QJsonObject Model::nozzelMerge(QJsonObject mergeItem, QJsonObject inputItem, QString appenditem) {
+    QJsonObject out;
+    mergeItem[appenditem + id] = inputItem[id];
+    mergeItem[appenditem + dmx] = inputItem[dmx];
+    mergeItem[appenditem + universe] = inputItem[universe];
+    return mergeItem;
+}
+
 QString Model::jsonTester() {
 //    qDebug() << "test";
-    return jsonData.toJson ();
+//    return jsonData.toJson ();
+    QJsonDocument resultmodel(jObj3DMap);
+    return resultmodel.toJson ();
+}
+
+QString Model::jsontreeTester() {
+    //    qDebug() << "test";
+    //    return jsonData.toJson ();
+    QJsonDocument resultmodel(jObjFountain);
+    return resultmodel.toJson ();
 }
 
 void Model::addObject() {
@@ -93,19 +245,21 @@ void Model::change_Rotation() {
     QJsonObject query(jsonData.object());
     QJsonObject Property;
     QJsonArray jArr(query["nozzle"].toArray());
-    Property = jArr[0].toObject();
-    Property["rX"] = rx;
-//    Property["rZ"] = 500;
-    rx += 10;
+
+    //    Property["rZ"] = 500;
+    rx += 3;
     if (rx > 90)
         rx = -90;
-    //    Property["x"] = 0;
-    //    Property["y"] = y;
-    //    y += 200;
-    //    qDebug() << " Y is:" << y;
-    //    Property["z"] = 0;
-    //    jArr.append(Property);
-    jArr[0] = Property;
+    for (int Loop = 0; Loop < jArr.count (); Loop++) {
+        Property = jArr[Loop].toObject();
+        if(Loop % 2)
+            Property["rX"] = rx;
+        else
+            Property["rX"] = -rx;
+        jArr[Loop] = Property;
+    }
     query["nozzle"] = jArr;
     jsonData.setObject(query);
 }
+
+
